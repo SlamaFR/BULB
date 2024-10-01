9c5242445f851fe91cecb36df4ef1b2a98a8f331
Irwin Madet <irwin.madet@gmail.com>
2024-10-01 13:41:27 +0200

    [BULB-0]: Disable ornament on small indices

--- components/editor/connection/mode/ConnectionLineEditor.vue
+++ components/editor/connection/mode/ConnectionLineEditor.vue
@@ -13,6 +13,11 @@ const emit = defineEmits<{
 const line = defineModel<ModeConnectionElement>('line', { required: true })
 
 const showOrnamentEditor = ref(false)
+
+function permittedTypes(mode: Mode | null) {
+  if (mode === 'BUS') return []
+  return undefined
+}
 </script>
 
 <template>
@@ -22,7 +27,7 @@ const showOrnamentEditor = ref(false)
         <IndexSelect v-model="line.$modeConnectionElement.lineIndex" :mode="mode" />
       </div>
       <div class="flex flex-row items-center gap-3">
-        <Checkbox v-model="line.$modeConnectionElement.walk" :input-id="line.id" binary />
+        <Checkbox v-model="line.$modeConnectionElement.walk" :input-id="line.id" :disabled="mode === 'BUS'" binary />
         <label :for="line.id" class="text-nowrap">Correspondance piétonne</label>
       </div>
 
@@ -32,6 +37,7 @@ const showOrnamentEditor = ref(false)
           size="small"
           label="Décoration"
           :severity="line.$modeConnectionElement.ornament ? 'primary' : 'secondary'"
+          :disabled="mode === 'BUS'"
           @click="showOrnamentEditor = true"
         />
         <Button
@@ -44,5 +50,9 @@ const showOrnamentEditor = ref(false)
     </div>
   </div>
 
-  <OrnamentEditor v-model="line.$modeConnectionElement.ornament" v-model:visible="showOrnamentEditor" />
+  <OrnamentEditor
+    v-model="line.$modeConnectionElement.ornament"
+    v-model:visible="showOrnamentEditor"
+    :permitted-types="permittedTypes(mode)"
+  />
 </template>
