c862a86f55951938b1c43acecb8efa348d1f260e
Irwin Madet <irwin.madet@gmail.com>
2024-10-02 17:33:14 +0200

    [BULB-0]: Add deadzone to line canvas

--- components/editor/LineCanvas.vue
+++ components/editor/LineCanvas.vue
@@ -22,29 +22,31 @@ onBeforeUnmount(() => exportSignal.off(doExport))
 
 <template>
   <div class="canvas">
-    <div class="flex-grow flex items-center overflow-y-auto">
-      <div ref="el" class="relative content bg-white pr-10em flex flex-row" :style="{ minHeight: `${mapSize}em` }">
-        <div class="flex flex-col min-w-fit">
-          <div class="w-full h-8 bg-[var(--blue-ratp-paper)]" />
-          <div class="p-3">
-            <div class="w-full flex flex-row gap-2 justify-center items-center text-3em">
-              <Mode :mode="mode" />
-              <LineIndex :index="index" />
+    <div class="flex flex-grow overflow-x-auto">
+      <div class="deadzone">
+        <div ref="el" class="relative content bg-white pr-10em flex flex-row" :style="{ minHeight: `${mapSize}em` }">
+          <div class="flex flex-col min-w-fit">
+            <div class="w-full h-8 bg-[var(--blue-ratp-paper)]" />
+            <div class="p-3">
+              <div class="w-full flex flex-row gap-2 justify-center items-center text-3em">
+                <Mode :mode="mode" />
+                <LineIndex :index="index" />
+              </div>
             </div>
           </div>
-        </div>
-        <SectionsGroup
-          v-model="topology"
-          class="w-max-content min-h-15em p-1em pt-20"
-        />
+          <SectionsGroup
+            v-model="topology"
+            class="w-max-content min-h-15em p-1em pt-20"
+          />
 
-        <div class="absolute bottom-.375em left-.375em text-[var(--blue-ratp-paper)]">
-          <div class="text-.2em flex flex-col line-height-1.75">
-            <span>Créé à l’aide de BULB • bulb.slama.io</span>
-            <span class="opacity-50 text-.5em">Version {{ version }}</span>
-            <hr class="my-.5">
-            <span class="text-.825em">Non affilié à la RATP, à Île-de-France Mobilités ou à toute autre société. Les pictogrammes ainsi que les polices utilisés demeurent la propriété intellectuelle exclusive des entités susmentionnées.</span>
-            <span class="italic text-.75em">Not affiliated with RATP, Île-de-France Mobilités or any other company. The pictograms and fonts used remain the exclusive intellectual property of the aforementioned entities.</span>
+          <div class="absolute bottom-.375em left-.375em text-[var(--blue-ratp-paper)]">
+            <div class="text-.2em flex flex-col line-height-1.75">
+              <span>Créé à l’aide de BULB • bulb.slama.io</span>
+              <span class="opacity-50 text-.5em">Version {{ version }}</span>
+              <hr class="my-.5">
+              <span class="text-.825em">Non affilié à la RATP, à Île-de-France Mobilités ou à toute autre société. Les pictogrammes ainsi que les polices utilisés demeurent la propriété intellectuelle exclusive des entités susmentionnées.</span>
+              <span class="italic text-.75em">Not affiliated with RATP, Île-de-France Mobilités or any other company. The pictograms and fonts used remain the exclusive intellectual property of the aforementioned entities.</span>
+            </div>
           </div>
         </div>
       </div>
@@ -68,8 +70,7 @@ onBeforeUnmount(() => exportSignal.off(doExport))
 
 <style scoped lang="scss">
 .canvas {
-  background-color: white;
-  border: 1px solid var(--blue-ratp-paper);
+  border: 1px solid var(--p-content-border-color);
   color: var(--p-gray-700);
   height: 100%;
 
@@ -77,11 +78,25 @@ onBeforeUnmount(() => exportSignal.off(doExport))
   flex-direction: column;
 }
 
+.deadzone {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 100%;
+  flex-grow: 1;
+  padding: 5em;
+  background: repeating-linear-gradient(45deg, var(--p-gray-200) 0, var(--p-gray-200) 1px, var(--p-gray-100) 1px,  var(--p-gray-100) 1em);
+  background-size: 100% 100%;
+}
+
 .content {
   font-size: var(--font-size);
   font-family: 'Parisine Std', sans-serif;
-  outline: 1px dashed var(--p-gray-200);
+  outline: 1px solid var(--p-gray-200);
   box-sizing: content-box;
+
+  overflow: hidden;
+  min-width: fit-content;
 }
 
 .toolbox {
