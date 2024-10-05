1a2cd81944de6ff3ba7a218b98561116852d09dd
Irwin Madet <irwin.madet@gmail.com>
2024-10-05 18:13:29 +0200

    [BULB-0]: Rework translations management

--- composables/useLocale.ts
+++ composables/useLocale.ts
@@ -1,12 +1,31 @@
 import { useI18n } from 'vue-i18n'
+import { en } from '~/assets/locales/en.json'
+import { fr } from '~/assets/locales/fr.json'
+import { ja } from '~/assets/locales/ja.json'
 
 export default function useLocale(fallback = 'en') {
   const preferredLanguages = usePreferredLanguages()
+  const { config } = usePrimeVue()
   const { locale: selectedLocale, availableLocales } = useI18n()
 
-  const locale = preferredLanguages.value.find(language => availableLocales.includes(language)) // try to find exact match
-    ?? preferredLanguages.value.find(language => availableLocales.includes(language.slice(0, 2)))?.slice(0, 2) // try to find match with only language code
-    ?? fallback // fallback
+  const locale = preferredLanguages.value.find(language => availableLocales.includes(language))
+    ?? preferredLanguages.value.find(language => availableLocales.includes(language.slice(0, 2)))?.slice(0, 2)
+    ?? fallback
+
+  watch(selectedLocale, (val) => {
+    switch (val) {
+      case 'fr':
+        config.locale = fr
+        break
+      case 'ja':
+        config.locale = ja
+        break
+      case 'en':
+      default:
+        config.locale = en
+        break
+    }
+  }, { immediate: true })
 
   const localeSetting = useLocalStorage('locale', locale)
   syncRef(localeSetting, selectedLocale)
